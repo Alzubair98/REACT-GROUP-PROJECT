d202e5203e61e236d7d32ee781718634be38c52c
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-24 16:53:29 -0400

    Add rockets reserved and missions joined to profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -3,19 +3,24 @@ import { useSelector } from 'react-redux';
 import ProfileCard from './profileCard';
 
 const Profile = () => {
-  const state = useSelector((state) => state.storeSlice);
-  const newState = state.filter((rocket) => rocket.value === true);
+  const rockets = useSelector((state) => state.storeSlice);
+  const reservedRockets = rockets.filter((rocket) => rocket.value === true);
+  const missions = useSelector((state) => state.missions);
+  const joinedMissions = missions.filter((mission) => mission.status === true);
 
   return (
     <div className="profile-cont">
-      <div>
-        mission
+      <div className="user-reserved">
+        <h2>My Missions</h2>
+        {joinedMissions.map((mission) => (
+          <ProfileCard key={mission.mission_id} mission={mission} id={mission.mission_id} name={mission.mission_name} className="leave-btn" text="Leave Mission" />
+        ))}
       </div>
 
-      <div className="rockets-profile">
-        <h1>My Rockets</h1>
-        {newState.map((rocket) => (
-          <ProfileCard key={rocket.id} name={rocket.name} />
+      <div className="user-reserved">
+        <h2>My Rockets</h2>
+        {reservedRockets.map((rocket) => (
+          <ProfileCard key={rocket.id} id={rocket.id} name={rocket.name} className="cancel-button" text="Cancel Reservation" />
         ))}
       </div>
     </div>
