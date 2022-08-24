0a7b26aca7024edcf99a6026bd96de41dd06502a
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-24 16:56:59 -0400

    Add cancel/leave buttons for missions/rockets to profile

--- src/components/profileCard.js
+++ src/components/profileCard.js
@@ -1,13 +1,38 @@
 import React from 'react';
-
+import { useDispatch } from 'react-redux';
 import propTypes from 'prop-types';
+import { joinMissions } from '../redux/Missions/missionsSlice';
+import { rocketAction } from '../redux/Rockets/rockets';
 
 const ProfileCard = (props) => {
-  const { name } = props;
+  const {
+    name, id, className, text,
+  } = props;
+
+  const dispatch = useDispatch();
+
+  const handleClick = (e) => {
+    if (e.target.className === 'leave-btn') {
+      dispatch(joinMissions({ id: e.target.id }));
+    } else {
+      const { id } = e.target;
+      dispatch(rocketAction.rocketReserve({ id }));
+    }
+  };
 
   return (
     <div className="profile-card">
       <h3>{name}</h3>
+      <div className="btn-container">
+        <button
+          type="button"
+          id={id}
+          className={className}
+          onClick={handleClick}
+        >
+          {text}
+        </button>
+      </div>
     </div>
   );
 };
@@ -16,7 +41,10 @@ export default ProfileCard;
 
 ProfileCard.propTypes = {
   name: propTypes.string,
-};
+  id: propTypes.string,
+  className: propTypes.string,
+  text: propTypes.string,
+}.isRequired;
 
 ProfileCard.defaultProps = {
   name: 'default',
