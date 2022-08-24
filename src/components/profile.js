9b5c8247b2cca34aa10e442de00d3ff3ec09e580
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-24 17:33:10 -0400

    Add placeholder message for profile missions/rockets

--- src/components/profile.js
+++ src/components/profile.js
@@ -11,15 +11,19 @@ const Profile = () => {
   return (
     <div className="profile-cont">
       <div className="user-reserved">
-        <h2>My Missions</h2>
-        {joinedMissions.map((mission) => (
+        <h2>My Missions<span>&#128377;</span></h2>
+        { joinedMissions.length === 0 ? 
+        <div id="empty-msg"><span>No missions joined</span></div> :
+        joinedMissions.map((mission) => (
           <ProfileCard key={mission.mission_id} mission={mission} id={mission.mission_id} name={mission.mission_name} className="leave-btn" text="Leave Mission" />
         ))}
       </div>
 
       <div className="user-reserved">
-        <h2>My Rockets</h2>
-        {reservedRockets.map((rocket) => (
+        <h2>My Rockets <span>&#128640;</span></h2>
+        { reservedRockets.length === 0 ?
+        <div id="empty-msg"><span>No rockets reserved</span></div> :
+        reservedRockets.map((rocket) => (
           <ProfileCard key={rocket.id} id={rocket.id} name={rocket.name} className="cancel-button" text="Cancel Reservation" />
         ))}
       </div>
