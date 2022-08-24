79403dcd894484232097ef9796a1332721a5b2f3
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-24 16:58:40 -0400

    Create/add snapshot tests for missions component

--- src/__test__/render.test.js
+++ src/__test__/render.test.js
@@ -2,14 +2,27 @@ import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import Rockets from '../components/Rockets';
+import RocketCard from '../components/RocketCard';
 import Profile from '../components/profile';
+import ProfileCard from '../components/profileCard';
+import Missions from '../components/Missions'
+import MissionItem from '../components/MissionItem'
 import store from '../redux/configureStore';
 
-describe('Rockets & Profile renderer test', () => {
+describe('Space App components rendering test', () => {
   it('match snapshot for Rockets', () => {
     const tree = TestRenderer.create(
       <Provider store={store}>
         <Rockets />
+      </Provider>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('match snapshot for the Missions', () => {
+    const tree = TestRenderer.create(
+      <Provider store={store}>
+        <Missions />
       </Provider>,
     ).toJSON();
     expect(tree).toMatchSnapshot();
@@ -23,4 +36,5 @@ describe('Rockets & Profile renderer test', () => {
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
+
 });
