56bb89d1007fcc182d989a9e1af01d4370432383
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-08-26 17:05:04 +0300

    fix linters errors

--- src/__test__/reducers.test.js
+++ src/__test__/reducers.test.js
@@ -1,12 +1,12 @@
-import getMissions from '../redux/Missions/missionsSlice';
-import storeSlice from '../redux/Rockets/rockets';
+import Mission from '../redux/Missions/missionsSlice';
+import Slice from '../redux/Rockets/rockets';
 
-describe('not', () => {
+describe('test the reducers to return the initial state', () => {
   it('test the storeSlice reducer for the mission to return empty array as initialState', () => {
-    expect(getMissions(undefined,{})).toEqual([]);
+    expect(Mission(undefined, {})).toEqual([]);
   });
 
   it('test the storeSlice reducer for the rockets to return empty array as initialState', () => {
-    expect(storeSlice(undefined, {})).toEqual([]);
+    expect(Slice(undefined, {})).toEqual([]);
   });
 });
